0ffe7d481a0b3547fd50ca0e290dbe6f6d32753b
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-11-05 20:54:58 +0700

    2

--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ import App from "./components/App";
 ReactDOM.render(<App />, document.getElementById("root"));
 
 const citrus = ["Lime", "Lemon", "Orange"];
+const fruits = ["Apple", "Banana", "Coconut", ...citrus];
+
+console.log(fruits);
